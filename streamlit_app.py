8ac21c8b6c049600820e645a9081865203e59da6
cdyer747 <54800418+cdyer747@users.noreply.github.com>
2024-11-04 16:33:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,11 @@ import csv
 
 ########################
 hide_st_style = """
+<style>
 MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
+</style>
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 #########################
